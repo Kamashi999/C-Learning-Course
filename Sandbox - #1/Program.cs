520732d95d746b712f1929a776caa9f715e68600
Sebastia n <KamashiVentures@gmail.com>
2025-02-07 17:52:57 +0100

    Sandbox Area Created

---
+++ Sandbox - #1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, choose option \n 1 - addition 1+1");
+
+string input = Console.ReadLine();
+
+int AddOnePlusOne()
+{
+	int a = 1;
+	int result = a + a;
+	return result;
+}
+
+
+
+switch (input)
+{
+	case "1":
+		Console.Clear();
+		Console.WriteLine($"Your number = {AddOnePlusOne()}");
+		break;
+	default:
+		break;
+}
