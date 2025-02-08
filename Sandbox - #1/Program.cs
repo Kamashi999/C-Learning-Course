b5602a4470f8b9302c45635aba146ce420a2cbe1
Sebastia n <KamashiVentures@gmail.com>
2025-02-08 13:42:19 +0100

    Sandbox Adjust Changed technique from using goto -> to  using while (true) instead.

--- Sandbox - #1/Program.cs
+++ Sandbox - #1/Program.cs
@@ -1,7 +1,4 @@
-﻿Console.WriteLine("Hello, choose option \n 1 - addition 1+1");
-
-string input = Console.ReadLine();
-
+﻿
 int AddOnePlusOne()
 {
 	int a = 1;
@@ -9,14 +6,47 @@ int AddOnePlusOne()
 	return result;
 }
 
-
-
-switch (input)
+while (true)
 {
-	case "1":
-		Console.Clear();
-		Console.WriteLine($"Your number = {AddOnePlusOne()}");
-		break;
-	default:
-		break;
+    Console.WriteLine("Hello, choose option \n 1 - addition 1+1");
+
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine($"Your number = {AddOnePlusOne()}");
+            break;
+        default:
+            Console.WriteLine("I told you to choose between 1-1!");
+            Console.ReadKey();
+            Console.Clear();
+            continue;
+    }
+    Console.ReadKey();
+
+    bool repeatWhile2 = true;
+    while (repeatWhile2)
+    {
+        Console.Clear();
+        Console.WriteLine("wanna repeat? \n 1 - Yes \n 2 - No");
+        string inputRepeat = Console.ReadLine();
+        if (inputRepeat == "1")
+        {
+            Console.WriteLine("Alright, let's go then!");
+            Console.ReadKey();
+           repeatWhile2 = false;
+            Console.Clear();
+        } else if (inputRepeat == "2")
+        {
+            return;
+        } else
+        {
+            Console.Clear();
+            Console.WriteLine("Sorry, I told you to choose between 1-2!");
+            Console.ReadKey();
+        }
+    }
 }
+
