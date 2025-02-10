29f90059af0bff826a0e0ed6e87833e78ea60af5
Sebastia n <KamashiVentures@gmail.com>
2025-02-10 20:26:58 +0100

    Program 3# - Database
    Something went wrong...

---
+++ Program - #3 User Database/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+
+Console.WriteLine("Welcome in my first Database program! Let's login.");
+
+// loop start
+string user1 = "QWERTY";
+string user2 = "QAZWSX";
+
+bool while1 = true;
+while (while1) {
+    Console.Clear();
+Console.WriteLine($"Choose account. \n 1 - {user1} \n 2 - {user2}");
+if (Int32.TryParse(Console.ReadLine(), out int input) && input == 1 || input == 2) {
+        while1 = false;
+
+        switch (input)
+        {
+            case 1: Console.WriteLine("Please, enter the password:");
+                string passwordInput = Console.ReadLine();
+                while (passwordInput != "1234")
+                {
+                    Console.WriteLine("Wrong password! Try again!");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                Console.WriteLine($"Hello {user1}! Choose option:\n 1 - Account info\n 2 - Date");
+                
+                bool caseWhile = true;
+                while (caseWhile) { 
+                if (Int32.TryParse(Console.ReadLine(), out int caseInput) && caseInput > 0 && caseInput < 3) {
+                    switch (caseInput)
+                        {
+                            case 1: 
+                                Console.WriteLine($"Username: {user1}");
+                                Console.WriteLine($"Created: 11.02.2019");
+                                Console.WriteLine($"Password: ****");
+                                Console.WriteLine($"Date of birth: 12.12.2002");
+                                break;
+                            case 2: 
+                                Console.WriteLine(DateTime.Now);
+                                Console.WriteLine("Wanna go back?\n (1 - Yes / 2 - No)");
+                                if (Console.ReadLine() == "1")
+                                {
+                                    caseWhile = true;
+                                } else
+                                {
+                                    Console.WriteLine("Bye Bye!");
+                                    Console.ReadKey();
+                                    return;
+                                }
+                                break;
+                        }
+                    }
+                }
+                
+                Console.WriteLine("test");
+                break;
+            case 2: Console.WriteLine("2");
+                break;
+                
+        }
+} else
+    {
+        Console.WriteLine("You should choose between 1-2!");
+        Console.ReadKey();
+    }
+}
--- Sandbox - #1/Program.cs
+++ Sandbox - #1/Program.cs
@@ -1,52 +1,75 @@
 ﻿
-int AddOnePlusOne()
+//int AddOnePlusOne()
+//{
+//	int a = 1;
+//	int result = a + a;
+//	return result;
+//}
+
+//while (true)
+//{
+//    Console.WriteLine("Hello, choose option \n 1 - addition 1+1");
+
+//    string input = Console.ReadLine();
+
+//    switch (input)
+//    {
+//        case "1":
+//            Console.Clear();
+//            Console.WriteLine($"Your number = {AddOnePlusOne()}");
+//            break;
+//        default:
+//            Console.WriteLine("I told you to choose between 1-1!");
+//            Console.ReadKey();
+//            Console.Clear();
+//            continue;
+//    }
+//    Console.ReadKey();
+
+//    bool repeatWhile2 = true;
+//    while (repeatWhile2)
+//    {
+//        Console.Clear();
+//        Console.WriteLine("wanna repeat? \n 1 - Yes \n 2 - No");
+//        string inputRepeat = Console.ReadLine();
+//        if (inputRepeat == "1")
+//        {
+//            Console.WriteLine("Alright, let's go then!");
+//            Console.ReadKey();
+//           repeatWhile2 = false;
+//            Console.Clear();
+//        } else if (inputRepeat == "2")
+//        {
+//            return;
+//        } else
+//        {
+//            Console.Clear();
+//            Console.WriteLine("Sorry, I told you to choose between 1-2!");
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+
+Console.WriteLine("Welcome, wanna try create your table?\nEnter amount of names u wanna have in your array:");
+
+int arraySize = Int32.Parse(Console.ReadLine());
+
+string[] names = new string[arraySize];
+
+
+for (int i = 0; i < names.Length; i++)
 {
-	int a = 1;
-	int result = a + a;
-	return result;
+    Console.WriteLine("write first name: ");
+    names[i] = Console.ReadLine();
 }
 
-while (true)
+Console.ReadKey();
+
+for (int i = 0; i < names.Length; i++)
 {
-    Console.WriteLine("Hello, choose option \n 1 - addition 1+1");
-
-    string input = Console.ReadLine();
-
-    switch (input)
-    {
-        case "1":
-            Console.Clear();
-            Console.WriteLine($"Your number = {AddOnePlusOne()}");
-            break;
-        default:
-            Console.WriteLine("I told you to choose between 1-1!");
-            Console.ReadKey();
-            Console.Clear();
-            continue;
-    }
-    Console.ReadKey();
-
-    bool repeatWhile2 = true;
-    while (repeatWhile2)
-    {
-        Console.Clear();
-        Console.WriteLine("wanna repeat? \n 1 - Yes \n 2 - No");
-        string inputRepeat = Console.ReadLine();
-        if (inputRepeat == "1")
-        {
-            Console.WriteLine("Alright, let's go then!");
-            Console.ReadKey();
-           repeatWhile2 = false;
-            Console.Clear();
-        } else if (inputRepeat == "2")
-        {
-            return;
-        } else
-        {
-            Console.Clear();
-            Console.WriteLine("Sorry, I told you to choose between 1-2!");
-            Console.ReadKey();
-        }
-    }
+    Console.WriteLine(names[i]);
 }
+
+
 
