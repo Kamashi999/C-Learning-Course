908dd88bb9d3c06747c7437b46c091f2b98766f8
Sebastia n <KamashiVentures@gmail.com>
2025-02-11 13:51:05 +0100

    Loop Foreach

---
+++ Command - Loop Foreach/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string[] names =
+{
+    "Anna", "Mark"
+};
+
+// Pętla foreach, działa na zasadzie stowrzenia jej własnej zmiennej, z której czyta wartości innej zmiennej.
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
