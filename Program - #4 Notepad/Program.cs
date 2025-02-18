c4649d86190667df066c76b5c9438be5c2ce3158
Sebastia n <KamashiVentures@gmail.com>
2025-02-18 16:56:02 +0100

    Notepad Adjust

--- Program - #4 Notepad/Program.cs
+++ Program - #4 Notepad/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-List<int> notepadForNumbers = new List<int>();
+
 
 void GreetingFunction()
 {
@@ -7,24 +7,13 @@ void GreetingFunction()
     while (welcomeWhile)
     {
         Console.Clear();
-        Console.WriteLine("Hello in notepad! \nWhat would you like to Note? 1 - Text / 2 - Numbers");
-        string welcomeOutput = Console.ReadLine();
-        if (welcomeOutput == "1" || welcomeOutput == "2")
-        {
-            welcomeWhile = false;
-            Console.ReadKey();
-        }
-        else
-        {
-            Console.WriteLine("choose between 1-2!");
-            Console.ReadKey();
-        }
+        Console.WriteLine("Hello in notepad! \nWhat would you like to Note? 1 - Text \n 2 - Numbers (Only Avaiable rn)");
     }
 }
 
 void NotepadForNumbers()
 {
-
+    List<int> notepadForNumbers = new List<int>();
 
     while (true)
     {
@@ -51,27 +40,60 @@ void NotepadForNumbers()
         {
 
             Console.WriteLine("Choose option.\n1 - Repeat.\n2 - show Notepad.\n3 - Close Program.");
-            if (Console.ReadLine() == "1")
+            string lobbyOutput = Console.ReadLine();
+
+            switch (lobbyOutput)
             {
-                Console.WriteLine("Lets go back then!");
-                Console.ReadKey();
-                lobbyBool = false;
-            }
-            else if (Console.ReadLine() == "2")
-            {
-                foreach (int numbers in notepadForNumbers)
-                {
-                    Console.WriteLine(numbers);
-                }
-                Console.ReadKey();
-            }
-            else if (Console.ReadLine() == "3")
-            {
-                Console.WriteLine("Thanks for spending your time with me!");
-                return;
+                case "1":
+                    Console.WriteLine("Lets go back then!");
+                    Console.ReadKey();
+                    lobbyBool = false;
+                    break;
+                case "2":
+                    foreach (int numbers in notepadForNumbers)
+                    {
+                        Console.Write(numbers + " ");
+                    }
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.WriteLine("Thanks for spending your time with me!");
+                    return;
+                default:
+                    Console.WriteLine("Choose between 1-3!");
+                    Console.ReadKey();
+                    break;
             }
         }
     }
 }
 
-NotepadForNumbers();
+void ChooseOptionFunction()
+{
+    bool lobbyBoolOutput = true;
+
+    while (lobbyBoolOutput)
+    {
+
+        string lobbyOutput = Console.ReadLine();
+
+        switch (lobbyOutput)
+        {
+            case "1":
+                break;
+            case "2":
+                lobbyBoolOutput = false;
+                NotepadForNumbers();
+                break;
+            default:
+                Console.ReadKey();
+                Console.Clear();
+                break;
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Hello in notepad! \nWhat would you like to Note?\n 1 - Text \n 2 - Numbers (Only Avaiable rn)");
+ChooseOptionFunction();
+
