48f6613d1959067a92da77565fd575cac713470c
Sebastia n <KamashiVentures@gmail.com>
2025-02-27 11:43:36 +0100

    Save datafiles into txt. sync way and async.

---
+++ Command - Save Data in txt/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+//sposób dodawania danych to plików .txt synchroniczny
+string fileName = "myfile.txt";
+
+//string path = @"files/" + fileName;
+
+string path = Path.Combine(@"files\", fileName);
+
+using (StreamWriter sw = new(path, true))
+{
+    sw.Write("Date: ");
+    sw.Write(DateTime.Now);
+    sw.Close();
+}
+
+//sposób dodawania danych to plików .txt asynchroniczny
+
+static async void WriteToFile(string msg)
+{
+    string fileName = "myfileASync.txt";
+    string path = Path.Combine(@"files\", fileName);
+    using(StreamWriter sw = new StreamWriter(path))
+    {
+        await sw.WriteLineAsync(msg);
+        sw.Close();
+    }
+}
+
+WriteToFile("Async Text");
