2918a3964a4cfd7faaf7e0e38c4c103fd9ef0d5a
Sebastia n <KamashiVentures@gmail.com>
2025-03-08 20:18:28 +0100

    Added comments

--- Command - Save Data in txt/Program.cs
+++ Command - Save Data in txt/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.VisualBasic;
 //sposób dodawania danych to plików .txt synchroniczny
-string fileName = "myfile.txt";
+string fileName = "myfile.txt"; // Nazwa pliku, do którego zapisujemy dane
 
 //string path = @"files/" + fileName;
 
-string path = Path.Combine(@"files\", fileName);
+string path = Path.Combine(@"files\", fileName); // tworzy ścieżkę do pliku.
 
-using (StreamWriter sw = new(path, true))
+using (StreamWriter sw = new(path, true)) // Otwiera plik w trybie dopisywania (append mode)
 {
     sw.Write("Date: ");
     sw.Write(DateTime.Now);
@@ -19,9 +19,9 @@ static async void WriteToFile(string msg)
 {
     string fileName = "myfileASync.txt";
     string path = Path.Combine(@"files\", fileName);
-    using(StreamWriter sw = new StreamWriter(path))
+    using(StreamWriter sw = new StreamWriter(path)) // Otwiera plik w trybie zapisu
     {
-        await sw.WriteLineAsync(msg);
+        await sw.WriteLineAsync(msg); // Asynchronicznie zapisuje tekst do pliku
         sw.Close();
     }
 }
